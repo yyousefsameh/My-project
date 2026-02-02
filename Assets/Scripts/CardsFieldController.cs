3f41702a3955f4273f1ab32b12115c31613cf129
yyousefsameh <yousefsameh2002@gmail.com>
2026-02-02 22:35:39 +0200

    Creating LoadAllPossibleCardsSprites function
    this function is to load all sprites in Resources folder to the allpossiblecardimages list

--- Assets/Scripts/CardsFieldController.cs
+++ Assets/Scripts/CardsFieldController.cs
@@ -4,7 +4,9 @@ using UnityEngine.UI;
 
 public class CardsFieldController : MonoBehaviour
 {
-    [SerializeField] private Sprite CardBack;
+    [SerializeField] private Sprite CardBackImage;
+    [SerializeField] private Sprite[] allpossibleCardImages;
+    [SerializeField] private List<Sprite> allpossiblecardImagesChosen = new();
     [SerializeField] private List<Button> CardsButtons = new();
     [SerializeField] private Transform CardsField;
 
@@ -33,18 +35,46 @@ public class CardsFieldController : MonoBehaviour
         for (int i = 0; i < cards.Length; i++)
         {
             CardsButtons.Add(cards[i].GetComponent<Button>());
-            CardsButtons[i].image.sprite = CardBack;
+            CardsButtons[i].image.sprite = CardBackImage;
         }
     }
+
+    //Click on a card
+    // private void ClickOnACard()
+    // {
+    //     foreach (Button cardButton in CardsButtons)
+    //     {
+    //         //when a card is clicked on it will call the function of OnCardClick
+    //         cardButton.onClick.AddListener(() => OnCardClick());
+    //     }
+    // }
+
+    // private void OnCardClick()
+    // {
+
+
+
+    // }
+
 
     // Start is called before the first frame update
     private void Awake()
     {
+        LoadAllPossibleCardsSprites();
         CreateCardsInCardsField();
     }
+
+    private void LoadAllPossibleCardsSprites()
+    {
+        allpossibleCardImages = Resources.LoadAll<Sprite>("Images/Sprites");
+    }
+
     void Start()
     {
         GetCards();
+        // ClickOnACard();
     }
+
+
 
 }
