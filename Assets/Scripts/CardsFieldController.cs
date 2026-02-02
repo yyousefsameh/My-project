985845eca5be4be5585fba74c67143f5bb91495a
yyousefsameh <yousefsameh2002@gmail.com>
2026-02-02 20:44:10 +0200

    Creating CreateCards to automally create number of cards
    creating CreateCardsInCardsField to make automatic cards rather than puting it manually in the game engine

--- Assets/Scripts/CardsFieldController.cs
+++ Assets/Scripts/CardsFieldController.cs
@@ -2,6 +2,30 @@ using UnityEngine;
 
 public class CardsFieldController : MonoBehaviour
 {
+    [SerializeField] private Transform CardsField;
+
+    [SerializeField] private GameObject CardPrefab;
+
+
+    private void CreateCardsInCardsField()
+    {
+        for (int i = 0; i < 6; i++)
+        {
+            //create a card  
+            GameObject card = Instantiate(CardPrefab);
+            // To give the card a name as a game object in hierarchy
+            card.name = "Card" + i;
+            //To Attach the card to the CardsField
+            card.transform.SetParent(CardsField, false);
+
+        }
+
+    }
+    // Start is called before the first frame update
+    private void Awake()
+    {
+        CreateCardsInCardsField();
+    }
 
 
 }
